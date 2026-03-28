ee93fab09c8f4e38c6752e0ab07b19e613a05102
rizwa <rizwa@DESKTOP-G8FBUJM>
2026-03-28 14:31:25 +0530

    Demo

---
+++ MavenProj/src/main/java/test/WebsiteTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebsiteTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver d=new ChromeDriver();
+d.get("https://google.com");
+	}
+
+}
